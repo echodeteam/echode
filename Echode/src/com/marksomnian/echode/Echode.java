f23904800311bfb0e37ce17851f19af9378334b2
MrKireko <mrkireko@gmail.com>
2013-09-24 16:33:11 +0200

    Update Echode.java
    + added kill command
    + added welcome message

--- Echode/src/com/marksomnian/echode/Echode.java
+++ Echode/src/com/marksomnian/echode/Echode.java
@@ -1,31 +1,43 @@
-package com.marksomnian.echode;
+package com;
 
 import java.util.Scanner;
 
 public class Echode {
 	static Scanner scan;
 	private static String in;
-
+	
 	/**
-	 * @param args
-	 */
+	* @param args
+	*/
 	public static void main(String[] args) {
 		scan = new Scanner(System.in);
-		mainLoop();
-
+		intro();
 	}
+	
+	//i couldn't get the welcome message right, so thats why i made this
+	public static void intro() {
+		System.out.println("Welcome to ECHODE version 0.2");
+		mainLoop();
+	}
+	
 	private static void mainLoop() {
-		while(true) {
+		while (true) {
 			System.out.print("->");
 			in = scan.nextLine();
 			parse(in);
 		}
 	}
+	
 	private static void parse(String in2) {
 		if (in2.equalsIgnoreCase("about")) {
-			System.out.println("Echode version 0.2\nMade by Erik Konijn and Marks Polakovs");
+			System.out.println("Echode version 0.2.2\nMade by Erik Konijn and Marks Polakovs");
 		} else {
-			System.out.println("Not implemented yet.");
+			if (in2.equalsIgnoreCase("kill")){
+				System.out.println("Echode shut down succesfully.");
+				System.exit(0);
+			}else{
+				System.out.println("Not implemented yet.");
+			}
 		}
 		
 	}
