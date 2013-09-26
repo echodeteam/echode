f88dab7b17ea4311bfd63f6eff8bb31c36cf8b88
MrKireko <mrkireko@gmail.com>
2013-09-26 14:35:42 +0200

    + time command 	+ time all ~ shows full time 	+ time date ~ shows date 	+ time digital ~ shows hh:mm:ss

--- Echode/src/com/base/echode/Echode.java
+++ Echode/src/com/base/echode/Echode.java
@@ -1,25 +1,27 @@
 package com.base.echode;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class Echode {
 	static Scanner scan;
 	private static String in;
-	
+
 	/**
-	* @param args
-	*/
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		scan = new Scanner(System.in);
 		intro();
 	}
-	
-	//welcome message
+
+	// welcome message
 	public static void intro() {
 		System.out.println("Welcome to ECHODE version 0.2.3");
 		mainLoop();
 	}
-	
+
 	private static void mainLoop() {
 		while (true) {
 			System.out.print("-> ");
@@ -27,44 +29,68 @@ public class Echode {
 			parse(in);
 		}
 	}
-	
+
 	private static void parse(String in2) {
 		/**
 		 * Commented this out, in case needed.
 		 * 
-		if (in2.equalsIgnoreCase("about")) {
-			System.out.println("Echode version 0.2.2\nMade by Erik Konijn and Marks Polakovs");
-		} else {
-			if (in2.equalsIgnoreCase("kill")){
-				System.out.println("Echode shut down succesfully.");
-				System.exit(0);
+		 * if (in2.equalsIgnoreCase("about")) { System.out.println(
+		 * "Echode version 0.2.2\nMade by Erik Konijn and Marks Polakovs"); }
+		 * else { if (in2.equalsIgnoreCase("kill")){
+		 * System.out.println("Echode shut down succesfully."); System.exit(0);
+		 * } else{ System.out.println("Not implemented yet."); } }
+		 **/
+		String[] result = in.split(" ", 2);
+		switch (result[0]) {
+		case "about":
+			System.out
+					.println("Echode version 0.2.3\nMade by Erik Konijn and Marks Polakovs");
+			break;
+		case "kill":
+			System.out.println("Echode shut down succesfully.");
+			System.exit(0);
+			break;
+		case "help":
+			System.out
+					.println("List of commands:\n"
+							+ "about ----------------------------------- Gives some info about ECHODE\n"
+							+ "help ---------------------------------------------- Lists all commands\n"
+							+ "kill ---------------------------------------- Quits the ECHODE console\n");
+			break;
+		case "version":
+			System.out.println("0.2.3");
+		case "time":
+			try {
+				switch (result[1]) {
+				case "all":
+					String alltime = new SimpleDateFormat(
+							"yyyy-MM-dd HH:mm:ss").format(Calendar
+							.getInstance().getTime());
+					System.out.println(alltime);
+					break;
+				case "date":
+					String datetime = new SimpleDateFormat(
+							"yyyy-MM-dd").format(Calendar
+							.getInstance().getTime());
+					System.out.println(datetime);
+					break;
+				case "digital":
+					String digitaltime = new SimpleDateFormat(
+							"HH:mm:ss").format(Calendar
+							.getInstance().getTime());
+					System.out.println(digitaltime);
+					break;
+				default:
+					System.out.println("Usage: 'time <all>'");
+					break;
+				}
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("Usage: 'time <all>'");
 			}
-		else{
-				System.out.println("Not implemented yet.");
-		}
-		}
-		**/
-		String word = in2.substring(0);
-		switch(word) {
-			case "about":
-				System.out.println("Echode version 0.2.3\nMade by Erik Konijn and Marks Polakovs");
-				break;
-			case "kill":
-				System.out.println("Echode shut down succesfully.");
-				System.exit(0);
-				break;
-			case "help":
-				System.out.println("List of commands:\n" + 
-						"about ----------------------------------- Gives some info about ECHODE\n" +
-						"help ---------------------------------------------- Lists all commands\n" +
-						"kill ---------------------------------------- Quits the ECHODE console\n");
-				break;
-			case "version":
-				System.out.println("0.2.3");
-			default:
-				System.out.println("Not implemented yet.");
-				break;
+			break;
+		default:
+			System.out.println("Not implemented yet.");
+			break;
 		}
 	}
-
 }
